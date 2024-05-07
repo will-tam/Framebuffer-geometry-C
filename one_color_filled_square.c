058ed16263d6a7ecf95d487905e749b5775f6a3d
will_tam <will_tam@club-internet.fr>
2024-05-08 00:41:47 +0200

    Add one_color_filled_square.*

---
+++ one_color_filled_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <linux/fb.h>
+
+#include "fb_design.h"
+#include "pixel.h"
+#include "one_color_filled_square.h"
+
+
+/******* Variables *******/
+extern struct framebuffer fb;
+extern struct pixel p;
+
+
+void one_color_filled_square()
+{
+  printf("Running one_color_filled_square()\n");
+  for (p.y = 0; (unsigned int)p.y < fb.vinfo.yres / 2; p.y++)
+  {
+    for (p.x = 0; (unsigned int)p.x < fb.vinfo.xres / 2; p.x++)
+    {
+      p.r = 255;
+      p.g = 255;
+      p.b = 255;
+      p.a = 0;
+      putpixel(fb, p);
+    }
+  }
+
+  printf("Final pxloffset = %ld\n", fb.pxloffset);
+  printf("/dev/fb0 has been painted successfully.\n\n");
+}
---
+++ one_color_filled_square.h
@@ -0,0 +1,12 @@
+/* 
+  Just fill a full white rectangle,
+  from point(0;0) to point(b.vinfo.xres / 2; b.vinfo.yres / 2).
+  First example to show how to display picture on FB.
+*/
+
+#ifndef _ONE_COLOR_FILLED_SQUARE_H
+#define _ONE_COLOR_FILLED_SQUARE_H
+
+void one_color_filled_square();
+
+#endif
