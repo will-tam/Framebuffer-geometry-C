e4b349f4bd2470a1c6ea8314839394be66a97cae
will_tam <will_tam@club-internet.fr>
2024-05-08 10:41:55 +0200

    Add empty_poly.*

---
+++ empty_poly.h
@@ -0,0 +1,9 @@
+#ifndef _EMPTY_POLY_H
+#define _EMPTY_POLY_H
+
+/* 
+  Just draw some polygons,
+*/
+void empty_poly();
+
+#endif
--- geometry.c
+++ geometry.c
@@ -36,8 +36,8 @@ Thanks to
 #include "circle.h"
 #include "one_color_filled_square.h"
 #include "empty_square.h"
-/*
 #include "empty_poly.h"
+/*
 #include "empty_circle.h"
 */
 
@@ -69,80 +69,6 @@ void set_non_canonical_mode(int ncm)
     tcgetattr(0, &info);
     info.c_lflag |= (ICANON|ECHO);
     tcsetattr(0, TCSANOW, &info);
-  }
-}
-
-void empty_poly()
-{
-  struct Coords {
-    int x0, y0;
-    int x1, y1;
-  } coords;
-
-  struct Coords polygone0[] = {
-    {0, 0, 100, 50},
-    {100, 50, 40, 100},
-    {40, 100, 0, 0}
-  };
-
-  struct Coords polygone1[] = {
-    {fb.vinfo.xres / 2 - 50, fb.vinfo.yres / 2 - 50, fb.vinfo.xres / 2, fb.vinfo.yres / 2 - 100},
-    {fb.vinfo.xres / 2, fb.vinfo.yres / 2 - 100, fb.vinfo.xres / 2 + 50 , fb.vinfo.yres / 2 - 50},
-    {fb.vinfo.xres / 2 + 50, fb.vinfo.yres / 2 - 50, fb.vinfo.xres / 2 + 50 , fb.vinfo.yres / 2 + 50},
-    {fb.vinfo.xres / 2 + 50, fb.vinfo.yres / 2 + 50, fb.vinfo.xres / 2 - 50 , fb.vinfo.yres / 2 + 50},
-    {fb.vinfo.xres / 2 - 50, fb.vinfo.yres / 2 + 50, fb.vinfo.xres / 2 - 50 , fb.vinfo.yres / 2 - 50},
-  };
-
-  struct Coords polygone2[] = {
-    {111, 104, 120, 50},
-    {120, 50, 216, 217},
-    {216, 217, 172, 198},
-    {172, 198, 148, 123},
-    {148, 123, 312, 104},
-    {312, 104, 111, 104},
-  };
-
-  struct Coords polygone3[] = {
-    {-20, 513, 613, 528},
-    {613, 528, 745, -50},
-    {745, -50, fb.vinfo.xres + 10 , fb.vinfo.yres + 20},
-    {fb.vinfo.xres + 10 , fb.vinfo.yres + 20, -20, 513},
-    
-  };
-
-  struct Coords *polygones[] = {
-    polygone0,
-    polygone1,
-    polygone2,
-    polygone3
-  };
-
-  unsigned int polynbpts[] = {
-    sizeof(polygone0) / sizeof(coords),
-    sizeof(polygone1) / sizeof(coords),
-    sizeof(polygone2) / sizeof(coords),
-    sizeof(polygone3) / sizeof(coords)
-  };
-
-  unsigned int i, j;    /* i = index of point to read for the j-th polygon. */
-  int x0, y0;           /* First point. */
-  int x1, y1;           /* Last point. */
-  
-  printf("Running empty_poly()\n");
-
-  for (j = 0; j < sizeof(polygones) / (sizeof(int*)); j++)
-  {
-    printf("%p ==> %d\n", polygones[j], polynbpts[j]);
-    for (i = 0; i < polynbpts[j]; i++)
-    {
-      printf("(%d;%d)-(%d,%d) / ", polygones[j][i].x0, polygones[j][i].y0, polygones[j][i].x1, polygones[j][i].y1);
-      x0 = polygones[j][i].x0;
-      y0 = polygones[j][i].y0;
-      x1 = polygones[j][i].x1;
-      y1 = polygones[j][i].y1;
-      line(x0, y0, x1, y1, 255, 255 * j * 9 / 10, 255 * j * 3 / 10, 0);
-    }
-    printf("\n\n");
   }
 }
 
